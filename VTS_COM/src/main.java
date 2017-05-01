c83c1f5c51b757f2f39e10643c1bb44954b9844e
Max Lundström <maxlundstrom@Jans-MacBook-Air.local>
2017-05-01 13:45:31 +0200

    em

--- VTS_COM/src/main.java
+++ VTS_COM/src/main.java
@@ -1,10 +1,10 @@
-
+import javax.swing;
 public class main {
 
-	public static void main(String [ ] args)
+	public static void main(String [] args)
 	{
 	      System.out.println("Sushi är Max");
-	      System.out.println("Max är Sushi");
+	      System.out.println("Max r Sushi");
 	}	
 	
 }
