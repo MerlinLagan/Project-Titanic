2c0f21278026120ce04be69fa310f3a643cacfc8
Max Lundström <maxlundstrom@Jans-MacBook-Air.local>
2017-05-04 11:29:54 +0200

    Update

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * Created by maxlundstrom on 04/05/17.
+ */
+public class main {
+
+    public static void main(String [] args)
+    {
+        System.out.println("Sushi är Max");
+        System.out.println("Max Sushi, (Oscar was here today)");
+    }
+
+}
