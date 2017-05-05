f9100ec6024dbea55499dd4c7907e81053027469
Max Lundström <maxlundstrom@Jans-MacBook-Air.local>
2017-05-05 09:26:20 +0200

    lagt till i main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,7 +9,7 @@ public class main {
     {
         System.out.println("Sushi är Max");
         System.out.println("Max Sushi, (Oscar was here today)");
-        System.out.print("Hej hej");
+        System.out.print("Hej hej hej jag är bara fjorton år");
     }
 
 }
