3854d142b7635199ed17bbe41ca7e29505856988
Jakob Larsson <jakob.lrssn@gmail.com>
2017-05-17 15:13:54 +0200

    changed main to big M

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,30 @@
-package main.java;
+// Vi vill kunna få timestamps / estimates vid olika polygoner och skapa en översikt av detta (info från flera aktörer)
+// Vi vill kunna vara redo innan ett fartyg ska kontakta oss
+// Vi vill kunna hitta de states som ex "entry_request" och "request_tO_switch_berth" och få upp detta
+// Vi vill kunna formatera Portcallmessages så att de funkar med input från frontenden
+// Vi vill kunna visa viktig info från olika meddelanden i frontenden
 
-import eu.portcdm.client.ApiClient;
-import eu.portcdm.client.service.PortcallsApi;
 
-/**
- * Created by maxlundstrom on 04/05/17.
- */
-public class main {
+public class Main {
+    public static void main(String[] args) {
+       // MessengerView hej = new MessengerView();
+       // hej.loadTemplatesMap("data.properties");
 
-    public static void main(String [] args)
-    public static void main( String[] args )
-    {
-        App app = new App();
-        app.initiateStateUpdateAPI();
-        PortCallMessage testPCM = app.testMessage("portcallone");
-        app.sendPCM(testPCM);
-        System.out.println("ran main method");
-        app.GetPortCalls();
+                MessagesHandlerModel msgsModel = new MessagesHandlerModel();
+                TemplatesHandlerModel msgrModel = new TemplatesHandlerModel();
+                PCMHandlerModel pcmHandler = new PCMHandlerModel();
+                PCMFetcherModel pcmFetcher = new PCMFetcherModel();
+                PCMSenderModel pcmSender = new PCMSenderModel();
+
+                Controller controller = new Controller();
+
+                MessengerView msgrView = new MessengerView(controller);
+                MessagesView msgsView = new MessagesView();
+
+                controller.addViews(msgrView, msgsView);
+
+                controller.addModels(msgrModel, msgsModel, pcmHandler, pcmFetcher, pcmSender);
+
+                controller.initialize();
     }
 }
-
