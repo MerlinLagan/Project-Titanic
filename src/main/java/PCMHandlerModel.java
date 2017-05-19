fc518ff52f2481e10268fdb46804e215b642637f
Oscar Sands <oscarsands@oscars-macbook-pro.local>
2017-05-19 18:55:26 +0200

    I PCMHandlerModel påbörjades en checkServiceState men kunde inte testas så då gjordes en checkComment som sedan testas i PCMHandlerModel main.
    checkServiceState kunde inte testa då jag hade svårt med att hitta vad för indata setServiceState(ServiceState) metoden skulle ta in.

--- src/main/java/PCMHandlerModel.java
+++ src/main/java/PCMHandlerModel.java
@@ -12,6 +12,10 @@ import eu.portcdm.messaging.*;
 import se.viktoria.stm.portcdm.connector.common.util.PortCallMessageBuilder;
 import se.viktoria.stm.portcdm.connector.common.util.StateWrapper;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import eu.portcdm.dto.LocationTimeSequence;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.List;
     public class PCMHandlerModel {
@@ -126,28 +130,99 @@ import java.util.List;
 
         }
 
-        //plocka ut en pmc, kolla om det är det xxx lägg in i ny lista "relavent list" reapat, lägg in if portcallid=xxx lägg in
-            public List<PortCallMessage> checkServiceState (List<PortCallMessage> messageList) {
-                for (PortCallMessage portCallMessage : messageList){
-                    if(portCallMessage.getPortCallId().toString() == "xxxx") {
-                        if (portCallMessage.getServiceState().toString() == "VTSAreaEntry_Requested")
-                            portCallMessage.getServiceState();
-                            System.out.print(portCallMessage);
-                        }
+        //Från en lista med alla PCM sorterar ut de som har relevant service state och returnerar de
+        public List<PortCallMessage> checkServiceState (List<PortCallMessage> messageList) {
+            List<PortCallMessage> relevantPCM = new ArrayList<>();
+
+            for (PortCallMessage portCallMessage : messageList) {
+                if (portCallMessage.getLocalPortCallId() == "urn:x-mrn:stm:portcdm:local_port_call:SEGOT:DHC:52724") {
+                    if (portCallMessage.getServiceState().toString() == "VTSAreaEntry_Requested") {
+                        relevantPCM.add(portCallMessage);
                     }
                 }
+            }
+            System.out.print(relevantPCM.toString());
             return null;
 
         }
 
+        //Samma uppbyggnad som checkServiceState men används för att testa i main
+         public static List<PortCallMessage> checkComment (List<PortCallMessage> messageList) {
+             List<PortCallMessage> relevantPCM= new ArrayList<>();
+             System.out.print("A");
+             for (PortCallMessage portCallMessage : messageList) {
+                 System.out.print("B");
+                 if (portCallMessage.getLocalPortCallId() == "urn:x-mrn:stm:portcdm:local_port_call:SEGOT:DHC:52724") {
+                     System.out.print("C");
+                     if (portCallMessage.getComment() == "Oscar") {
+                         System.out.print("D");
+                         relevantPCM.add(portCallMessage);
+                     }
+                 }
+             }         
+             System.out.print("E");
+             System.out.print(relevantPCM.toString());
+             return null;
+
+         }
+
         public List<String> getVesselTravelInfo(PortCallMessage message){
-            List<String> vesselInfo = null;
+            List<String> vesselInfo = new ArrayList<>(); ;
 
             vesselInfo.add(message.getLocationState().getArrivalLocation().getTo().getLocationType().toString());
             vesselInfo.add(message.getVesselId());
             vesselInfo.add(message.getLocationState().getTimeType().toString());
             return vesselInfo;
         }
+
+
+
+        private PortCallMessage createGenericMessage(LocationTimeSequence locationTimeSequence, LogicalLocation logicalLocation) {
+            StateWrapper stateWrapper = new StateWrapper(
+                    LocationReferenceObject.VESSEL, //referenceObject
+                    locationTimeSequence, //ARRIVAL_TO or DEPARTURE_FROM
+                    logicalLocation, //Type of required location
+                    53.50, //Latitude of required location
+                    53.50, //Longitude of required location
+                    "Skarvik Harbour 518", //Name of required location
+                    LogicalLocation.ANCHORING_AREA, //Type of optional location
+                    52.50, //Latitude of optional location
+                    52.50, //Longitude of optional location
+                    "Dana Fjord D1" );//Name of optional location
+            //Change dates from 2017-03-23 06:40:00 to 2017-03-23T06:40:00Z
+            PortCallMessage portCallMessage = PortCallMessageBuilder.build(
+                    "urn:x-mrn:stm:portcdm:local_port_call:SEGOT:DHC:52724", //localPortCallId
+                    "urn:x-mrn:stm:portcdm:local_job:FENIX_SMA:990198126", //localJobId
+                    stateWrapper, //StateWrapper created above
+                    "2017-03-23T06:40:01Z", //Message's time
+                    TimeType.ESTIMATED, //Message's timeType
+                    "urn:x-mrn:stm:vessel:IMO:9259501", //vesselId
+                    "2017-03-23T06:38:57Z", //reportedAt (optional)
+                    "Viktoria", //reportedBy (optional)
+                    "urn:x-mrn:stm:portcdm:message:5eadbb1c-6be7-4cf2-bd6d-f0af5a0c35dc", //groupWith (optional), messageId of the message to group with.
+                    "example comment" //comment (optional)
+            );
+            return portCallMessage;
         }
+
+        
+
+
+        //main för att testa checkComment som kommer fungerar typ som checkServiceState i framtiden
+        public static void main(String[] args) {
+
+            List<PortCallMessage> messageList = new ArrayList<>();
+
+            PortCallMessage message1 = new PCMHandlerModel().createGenericMessage(LocationTimeSequence.ARRIVAL_TO, LogicalLocation.ANCHORING_AREA);
+            message1.setComment("Oscar");
+            messageList.add(message1);
+
+            PortCallMessage message2 = new PCMHandlerModel().createGenericMessage(LocationTimeSequence.ARRIVAL_TO, LogicalLocation.BERTH);
+            messageList.add(message2);
+
+            checkComment(messageList);
+
+        }
+    }
 
 
